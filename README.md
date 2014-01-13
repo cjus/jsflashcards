06ce04ef960f53826c4f71e7f40f0c7eddc165bc
cjus <Carlos.Justiniano@gmail.com>
2014-01-13 05:27:23 -0800

    Cleanup and format improvements. Updated README.md with flashcard format and conventions.

--- README.md
+++ README.md
@@ -1,9 +1,26 @@
-JSFlashCards
-==========================
+# JSFlashCards
 
 JSFlashCards is a simple jQueryMobile app which allows you to test your JavaScript knowledge.
 
 To view a demo point your mobile web browser to:
 http://cjus.me/jsflashcards
 
-JSFlashCards uses John Fraser's showdown, a JavaScript port of Markdown. Flash cards are exists in a JSON file containing markdown text for both questions and answers. Ths makes it possible to easily author HTML based flash cards.
+JSFlashCards uses John Fraser's showdown, a JavaScript port of Markdown. Flash cards are exists in a JSON file containing markdown text for both questions and answers. This makes it possible to easily author HTML based flash cards.
+
+## Flashcard format
+
+A flashcard consist of an object entry in the js/cards.js file.
+
+    {
+        "question": "### How can you use primitive types to create objects?",
+        "answer": "Using the `new` operator:||    var numObject = new Number(10);|    console.log(typeOf numObject);|    > \"object\""
+    },
+
+Each flashcard object consists of a question and answer key followed by a single line string containing markdown text.
+In order to keep the file neat, markdown is formatted on a single line. The pipe (|) separator is later converted into a newline to render multi-line entries.
+
+The following conventions are observed:
+
+* all questions being with a triple hash (###) to set a consistent font size
+* all JS keywords are surrounded with tick marks (`) for readability
+
--- js/cards.js
+++ js/cards.js
@@ -5,35 +5,35 @@ cards = [
   },
   {
     "question": "### Name the JavaScript primitive types.",
-    "answer": "* Boolean, Number, String."
+    "answer": "`Boolean`, `Number`, `String`."
   },
   {
-    "question": "### What can you tell me about the JavaScript **'this'** keyword?",
-    "answer": "Generally it refers to the object which owns the method in which the **this** keyword is referenced.||However, it depends on how a function is called. If called from a global scope then **this** refers to the window object. If referenced within a constructor function than **this** refers to the newly created object. When referenced within an event handler then the **this** keyword refers to the object which called the event."
+    "question": "### What can you tell me about the JavaScript `this` keyword?",
+    "answer": "Generally it refers to the object which owns the method in which the `this` keyword is referenced.||However, it depends on how a function is called. If called from a global scope then `this` refers to the window object. If referenced within a constructor function than `this` refers to the newly created object. When referenced within an event handler then the `this` keyword refers to the object which called the event."
   },
   {
     "question": "### Name the JavaScript special types.",
-    "answer": "null and unassigned."
+    "answer": "`null` and `unassigned`."
   },
   {
     "question": "### Tell me about numbers in JavaScript.",
     "answer": "Number is a 64-bit floating point value, similar to Java's double. There is no integer type."
   },
   {
-    "question": "### How are undefined and null different?",
-    "answer": "Variables which have not been initialized are undefined, while variables which have an empty value are set to null."
+    "question": "### How are `undefined` and `null` different?",
+    "answer": "Variables which have not been initialized are `undefined`, while variables which have an empty value are set to `null`."
   },
   {
     "question": "### Tell me about strings in JavaScript.",
-    "answer": "String is a sequence of zero or more Unicode characters. There is no separate character type. A character is represented as a string length 1"
+    "answer": "`String` is a sequence of zero or more Unicode characters. There is no separate character type. A character is represented as a string length 1."
   },
   {
     "question": "### People say that almost everything in JavaScript is an object. What things are not objects?",
-    "answer": "Primitive types such as boolean, number and string. Also, special types as null and unassigned."
+    "answer": "Primitive types such as boolean, number and string. Also, special types as `null` and `unassigned`."
   },
   {
     "question": "### How can errors be handled in JavaScript?",
-    "answer": "Using try/catch/finally blocks."
+    "answer": "Using `try`/`catch`/`finally` blocks."
   },
   {
     "question": "### What is meant by the statement: JavaScript is loosely typed?",
@@ -48,7 +48,7 @@ cards = [
     "answer": "Brendan Eich, the creator of JavaScript."
   },
   {
-    "question": "### Which of these best describes the void keyword?|1. A method|2. A function|3. An operator|4. A statement",
+    "question": "### Which of these best describes the `void` keyword?|1. A method|2. A function|3. An operator|4. A statement",
     "answer": "Number #3, An operator"
   },
   {
@@ -57,15 +57,15 @@ cards = [
   },
   {
     "question": "### What are the nine native object constructors in JavaScript?",
-    "answer": "Number String, Boolean, Object, Array, Function, Date, Regex, Error."
+    "answer": "`Number`, `String`, `Boolean`, `Object`, `Array`, `Function`, `Date`, `Regex`, `Error`."
   },
   {
-    "question": "### Why can you **new Date** but can't **new Math**?",
-    "answer": "Date has a built-in object constructor but Math is just an object namespace setup by JavaScript to house math functions."
+    "question": "### Why can you `new Date` but can't `new Math`?",
+    "answer": "`Date` has a built-in object constructor but `Math` is just an object namespace setup by JavaScript to house math functions."
   },
   {
     "question": "### How are global variables created?",
-    "answer": "Global variables are created when you don't use the var keyword."
+    "answer": "Global variables are created when you don't use the `var` keyword."
   },
   {
     "question": "### How does JavaScript implement inheritance?",
@@ -73,7 +73,7 @@ cards = [
   },
   {
     "question": "### How can you use primitive types to create objects?",
-    "answer": "Using the new operator:||    var numObject = new Number(10);|    console.log(typeOf numObject);|    > \"object\""
+    "answer": "Using the `new` operator:||    var numObject = new Number(10);|    console.log(typeOf numObject);|    > \"object\""
   },
   {
     "question": "### What was JavaScript called before it was first released?",
@@ -93,7 +93,7 @@ cards = [
   },
   {
     "question": "### How does scoping in JavaScript differ from other programming languages?",
-    "answer": "JavaScript has function scoping (although, block scoping using the 'let' keyword was added in JavaScript 1.7). Most other programming languages support block scoping.||ref: [Wikipedia](http://en.wikipedia.org/wiki/JavaScript)"
+    "answer": "JavaScript has function scoping (although, block scoping using the `let` keyword was added in JavaScript 1.7). Most other programming languages support block scoping.||ref: [Wikipedia](http://en.wikipedia.org/wiki/JavaScript)"
   },
   {
     "question": "### Describe JavaScript objects.",
@@ -104,7 +104,11 @@ cards = [
     "answer": "Number 4. Harmony."
   },
   {
-    "question": "### Using a variable without prefixing it with the var keyword causes which of the following:||1. Creates a local variable|2. Generates and error.|3. Creates and array|4. Creates a global variable.",
+    "question": "### Using a variable without prefixing it with the `var` keyword causes which of the following:||1. Creates a local variable|2. Generates and error.|3. Creates and array|4. Creates a global variable.",
     "answer": "Number 4. Creates a global variable."
+  },
+  {
+    "question": "### The `Array` Constructor.",
+    "answer": "||ref: [JavaScript Garden](https://github.com/BonsaiDen/JavaScript-Garden)"
   }
 ];
